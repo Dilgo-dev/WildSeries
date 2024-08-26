a94f3fea67685133a04ad8b0b8a957aaad1f95a8
Benjamin <dilgopierre@gmail.com>
2024-08-26 16:14:59 +0200

    :hammer: Add body-parser to app

--- server/app/config.js
+++ server/app/config.js
@@ -1,10 +1,13 @@
 // Load the express module to create a web application
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const apiRouter = require("./routers/api/router");
+
+app.use(bodyParser.json())
 
 app.use("/api", apiRouter);
 
